96684c6ee98eb5cd9faacaca213275e9a2cbfab3
apalermo <anthony.palermo@laposte.net>
2025-12-24 10:51:36 +0100

    chore: setup global routing and root application layout

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/auth/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/auth/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+  },
+  {
+    path: 'me',
+    loadComponent: () =>
+      import('./pages/me/me.component').then((m) => m.MeComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
