18a42636d251e1a7e799a86175879cb26e942b28
apalermo <anthony.palermo@laposte.net>
2025-12-29 15:08:44 +0100

    refactor(home): simplify navigation using routerLink and update tests

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,21 +1,13 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   private readonly router = inject(Router);
-
-  public navigateToLogin(): void {
-    this.router.navigate(['/login']);
-  }
-
-  public navigateToRegister(): void {
-    this.router.navigate(['/register']);
-  }
 }
