b5dbf943ff271b1af0d749f6f79ecaaa78fe89c8
apalermo <anthony.palermo@laposte.net>
2026-01-06 14:41:08 +0100

    feat(articles): add CommentResponse and ArticleResponse DTOs for API output

--- back/src/main/java/com/openclassroom/mddapi/dtos/articles/ArticleResponse.java
+++ back/src/main/java/com/openclassroom/mddapi/dtos/articles/ArticleResponse.java
@@ -1,7 +1,10 @@
 package com.openclassroom.mddapi.dtos.articles;
 
+<<<<<<< HEAD
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+=======
+>>>>>>> 386d206 (feat(articles): add CommentResponse and ArticleResponse DTOs for API output)
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,13 +17,21 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+<<<<<<< HEAD
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+=======
+>>>>>>> 386d206 (feat(articles): add CommentResponse and ArticleResponse DTOs for API output)
 public class ArticleResponse {
     private Long id;
     private String title;
     private String content;
     private String authorName;
+<<<<<<< HEAD
     private ThemeResponse theme;
+=======
+    private Long themeId;
+    private String themeName;
+>>>>>>> 386d206 (feat(articles): add CommentResponse and ArticleResponse DTOs for API output)
     private List<CommentResponse> comments;
     private LocalDateTime createdAt;
 }
--- back/src/main/java/com/openclassroom/mddapi/dtos/articles/CommentResponse.java
+++ back/src/main/java/com/openclassroom/mddapi/dtos/articles/CommentResponse.java
@@ -1,7 +1,10 @@
 package com.openclassroom.mddapi.dtos.articles;
 
+<<<<<<< HEAD
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+=======
+>>>>>>> 386d206 (feat(articles): add CommentResponse and ArticleResponse DTOs for API output)
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,7 +16,10 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+<<<<<<< HEAD
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+=======
+>>>>>>> 386d206 (feat(articles): add CommentResponse and ArticleResponse DTOs for API output)
 public class CommentResponse {
     private Long id;
     private String content;
