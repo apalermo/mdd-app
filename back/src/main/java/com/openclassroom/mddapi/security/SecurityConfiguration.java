9589381f913ad65e8f8afaf06164a61504685b95
apalermo <anthony.palermo@laposte.net>
2026-01-07 17:55:53 +0100

    fix(config): allow swagger endpoints and correct jackson naming strategy

--- back/src/main/java/com/openclassroom/mddapi/security/SecurityConfiguration.java
+++ back/src/main/java/com/openclassroom/mddapi/security/SecurityConfiguration.java
@@ -27,7 +27,11 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exceptions -> exceptions
