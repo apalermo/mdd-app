538e8ef2cebee94135dfb93bf18b03d5b767b76a
apalermo <anthony.palermo@laposte.net>
2026-01-09 12:44:48 +0100

    refactor(back): harden security and unify auth error handling

---
+++ back/src/main/java/com/openclassroom/mddapi/exceptions/BadCredentialsException.java
@@ -0,0 +1,7 @@
+package com.openclassroom.mddapi.exceptions;
+
+public class BadCredentialsException extends RuntimeException {
+    public BadCredentialsException(String message) {
+        super(message);
+    }
+}
