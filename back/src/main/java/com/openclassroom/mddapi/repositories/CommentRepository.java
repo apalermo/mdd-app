47ec2bf5d628b86751bfca33373d9fed54e33d1f
apalermo <anthony.palermo@laposte.net>
2026-01-13 18:05:51 +0100

    docs(repo): technical javadoc for comment and subscription repositories

--- back/src/main/java/com/openclassroom/mddapi/repositories/CommentRepository.java
+++ back/src/main/java/com/openclassroom/mddapi/repositories/CommentRepository.java
@@ -4,7 +4,10 @@ import com.openclassroom.mddapi.entities.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for {@link Comment} entities.
+ * Manages developer feedback and peer collaboration interactions.
+ */
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-   
 }
