5a58b6784a6faf8c40efb25e2ca5c195d1ccd948
apalermo <anthony.palermo@laposte.net>
2026-01-14 12:26:31 +0100

    chore(e2e): implement resetDb task with mysql2 in cypress config

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -1,12 +1,35 @@
 import { defineConfig } from 'cypress';
+import * as mysql from 'mysql2/promise';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
-    supportFile: 'cypress/support/e2e.ts',
-    specPattern: 'cypress/e2e/**/*.cy.ts',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        async resetDb() {
+          const connection = await mysql.createConnection({
+            host: 'localhost',
+            user: config.env.DB_USER,
+            password: config.env.DB_PASSWORD,
+            database: config.env.DB_NAME,
+            multipleStatements: true,
+          });
+
+          try {
+            const sqlPath = path.join(__dirname, 'cypress/data/reset-db.sql');
+            const sql = fs.readFileSync(sqlPath, 'utf8');
+            await connection.query(sql);
+            return null;
+          } catch (err) {
+            console.error('Erreur Task resetDb:', err);
+            throw err;
+          } finally {
+            await connection.end();
+          }
+        },
+      });
     },
   },
 });
